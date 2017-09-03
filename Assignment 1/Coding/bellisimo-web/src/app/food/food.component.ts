6b4df37cdb6447924837c78103ec8376a69899e7
Diana <diana.kawuma@gmail.com>
2017-09-03 23:32:27 +0200

    added food interface

--- Assignment 1/Coding/bellisimo-web/src/app/food/food.component.ts
+++ Assignment 1/Coding/bellisimo-web/src/app/food/food.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Location} from '@angular/common';
+import { Directive,ElementRef, Input} from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-food',
   templateUrl: './food.component.html',
   styleUrls: ['./food.component.css']
 })
-export class FoodComponent implements OnInit {
+export class FoodComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  title = 'Food';
+  constructor(private location: Location) { }
+ 
+    goBack() {
+        this.location.back();
+    }
+    refresh(){
+      window.location.reload();
+    }
 
 }
+@Directive({ selector: '[myTest]' })
+export class TestDirective{
+  contructor(){
+      $(document).ready(function(){ 
+    $(".bt-menu-trigger").toggle( 
+    function(){
+      $('.bt-menu').addClass('bt-menu-open'); 
+    }, 
+    function(){
+      $('.bt-menu').removeClass('bt-menu-open'); 
+    } 
+  ); 
+}) 
+  }
+}
+
+ 
